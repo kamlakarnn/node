8106b4afe86ae52d2b15f12775b94d7f038c2076
kamlakarnn <kamalakar.gpt@gmail.com>
2026-01-08 22:43:13 +0530

    Fix src folder

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/detail/:user_id",(req,res)=>{
+    //console.log(req.query);
+
+    console.log(req.params.user_id);
+    
+    
+    res.send("get the details of data")
+})
+
+app.post("/insert",(req,res)=>{
+    res.send("inserted data successfully")
+})
+
+app.delete("/delete",(req,res)=>{
+    res.send("delete the data sucessfully")
+})
+
+app.use("/",(req,res)=>{
+    res.send("hellowesefweg")
+})
+
+
+
+app.listen(3000,()=>{
+    console.log("listing port 3000");
+    
+});
