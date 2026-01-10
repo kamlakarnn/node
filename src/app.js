c9713a0673a25a788679e5e241559e176f774490
kamlakarnn <kamalakar.gpt@gmail.com>
2026-01-10 11:33:00 +0530

    addin g and play with midleware i

--- src/app.js
+++ src/app.js
@@ -2,34 +2,47 @@ const express = require("express")
 
 const app = express()
 
-app.get("/detail/:user_id",(req,res)=>{
-    //console.log(req.query);
+// app.get("/detail/:user_id",(req,res)=>{
+//     //console.log(req.query);
 
-    console.log(req.params.user_id);
+//     console.log(req.params.user_id);
     
     
-    res.send("get the details of data")
-})
+//     res.send("get the details of data")
+// })
 
-app.post("/insert",(req,res)=>{
-    res.send("inserted data successfully")
-})
+// app.post("/insert",(req,res)=>{
+//     res.send("inserted data successfully")
+// })
 
-app.delete("/delete",(req,res)=>{
-    res.send("delete the data sucessfully")
-})
+// app.delete("/delete",(req,res)=>{
+//     res.send("delete the data sucessfully")
+// })
 
-app.get("/data",(req,res)=>{
-    res.send("data sucessfully")
-})
+// app.get("/data",(req,res)=>{
+//     res.send("data sucessfully")
+// })
 
-app.use("/",(req,res)=>{
-    res.send("hellowesefweg")
-})
+// app.use("/",(req,res)=>{
+//     res.send("hellowesefweg")
+// })
 
+app.use("/",
+    
+    (req,res , next)=>{
+      res.send("hellowesefweg")
 
+    console.log("hi ist req handler");
+    
+ next();
+},
 
-app.listen(3000,()=>{
-    console.log("listing port 3000");
+    (req,res)=>{
+    res.send("hello i am 2 nd handler")
+
+    })
+
+app.listen(4000,()=>{
+    console.log("listing port 4000");
     
 });
