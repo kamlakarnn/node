0accf7609fec2777466cf679b725bbc9851b4f9e
kamlakarnn <kamalakar.gpt@gmail.com>
2026-01-13 13:02:03 +0530

    adding data sanitization in db chema and add api patch is updatated to api level validator

--- src/app.js
+++ src/app.js
@@ -2,95 +2,104 @@ const express = require("express");
 
 const app = express();
 
-const {userAuth} = require("./middleware/auth")
+const { userAuth } = require("./middleware/auth");
 
-const connectDB = require("./config/database")
+const connectDB = require("./config/database");
 
 const User = require("./model/user");
 const { get } = require("mongoose");
 
-app.use(express.json());  // middlware to convert json to object
-
-
+app.use(express.json()); // middlware to convert json to object
 
 app.post("/insert", async (req, res) => {
+  console.log(req.body);
 
-    console.log(req.body);
+  const user = new User(req.body);
 
-    const user = new User(req.body);
-    
-//   const userobj = {
-//     name: "kamalakar",
-//     email: "kamlakar@external,com",
-//     password: "password123",
-// }
-//     const user = new User(userobj);
+  //   const userobj = {
+  //     name: "kamalakar",
+  //     email: "kamlakar@external,com",
+  //     password: "password123",
+  // }
+  //     const user = new User(userobj);
 
-    try {
+  try {
     await user.save();
     res.send("user inserted successfully");
-    } catch (error) {
-    res.status(500).send("error inserting user"+error.message );
-    }
-})
+  } catch (error) {
+    res.status(500).send("error inserting user" + error.message);
+  }
+});
 
 app.get("/getusers", async (req, res) => {
-    try {
-        const users = await User.find({});  
-        // res.send(users);
-        res.json(users);
-    } catch (error) {
-        res.status(500).send("error getting users"+ error.message);
-    }
-
+  try {
+    const users = await User.find({});
+    // res.send(users);
+    res.json(users);
+  } catch (error) {
+    res.status(500).send("error getting users" + error.message);
+  }
 });
 
-app.get("/find" , async (req,res) => {
-    try {
-         console.log(req.body);
-        const user = await User.findOne(req.body);
-       if (!user) {
-        return res.status(404).send("user not found");
-       }
-        
+app.get("/find", async (req, res) => {
+  try {
+    console.log(req.body);
+    const user = await User.findOne(req.body);
+    if (!user) {
+      return res.status(404).send("user not found");
+    }
+
     res.json(user);
-    } catch (error) {
-        res.status(500).send("error finding user"+ error.message);
-    }
-})
+  } catch (error) {
+    res.status(500).send("error finding user" + error.message);
+  }
+});
 
-app.delete("/deleteUser" , async (req,res) => {
+app.delete("/deleteUser", async (req, res) => {
+  try {
+    const user_id = req.body._id;
+    console.log(user_id);
+    const result = await User.deleteOne(user_id);
+    res.send("user deleted successfully");
+  } catch (error) {
+    res.status(500).send("error deleting user" + error.message);
+  }
+});
+
+app.patch("/updateUser", async (req, res) => {
+    const user_id = req.body.user_id;
+    const updateData = req.body;
+ 
     try {
-        const user_id = req.body._id;
-         console.log(user_id);
-        const result = await User.deleteOne(user_id);
-      res.send("user deleted successfully");
-       }  catch (error) { 
-        res.status(500).send("error deleting user"+ error.message);
+        // Validate allowed updates -- api level validation to check what we can update
+    const allowedUpdates = ['user_id','name', 'age', 'gender','password'];
+     const isupdateAllowed = Object.keys(updateData).every((k) =>
+      allowedUpdates.includes(k)
+    );
+    console.log(isupdateAllowed);
+    
+    if (!isupdateAllowed) {
+      throw new Error("update not allowed");
     }
-})   
+    
+    
+    console.log(user_id);
+    const result = await User.updateOne({ _id: user_id }, updateData, {
+      runValidators: true,  // to run the validators defined in the schema
+    });
+    res.send("user updated successfully");
+  } catch (error) {
+    res.status(500).send("error updating user" +  error.message);
+  }
+});
 
-app.patch("/updateUser" , async (req,res) => {
-    try {
-        const user_id = req.body.user_id;
-        const updateData = req.body;
-         console.log(user_id);
-        const result = await User.updateOne({_id: user_id},  updateData);
-          res.send("user updated successfully");
-       }  catch (error) { 
-        res.status(500).send("error updating user"+ error.message);
-    }
-})
-
-connectDB()  // Start the server after establishing database connection
-.then(() => {
+connectDB() // Start the server after establishing database connection
+  .then(() => {
     console.log("Database connected successfully");
     app.listen(4000, () => {
-  console.log("listing port 4000");
-});
-
-})
-.catch((err)=>{
+      console.log("listing port 4000");
+    });
+  })
+  .catch((err) => {
     console.error("Database connection failed:", err);
-})
-
+  });
--- src/model/user.js
+++ src/model/user.js
@@ -1,17 +1,56 @@
 const mongoose = require('mongoose');
 
+const validator = require('validator');
+
 
 const userSchema = new mongoose.Schema({
     name: {
-        type: String,   
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50   
     },
     email: {
         type: String,
+        required: true,
+        unique: true,  // check the whether email is unique or not
+        trim: true, // remove spaces
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid email format' + value);
+            }
+        }
     },
+     age: {
+        type: Number,
+        min: 5,
+    },
+    gender: {
+        type: String,
+        validate(value) {
+            if (!['male', 'female', 'other'].includes(value)) {
+                throw new Error('Invalid gender value' + value );
+            }  
+        }
+    },
+
+    skills: {
+        type: [String], // array of strings
+        
+    },
+            
     password: {
         type: String,
+        required: true,
+        minlength: 6,
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error('Password is not strong enough'+ value);
+
+            }
+        }
     }
-});
+}, { timestamps: true } );
 
 //create modelc 
 // const User = mongoose.model('User', userSchema);
